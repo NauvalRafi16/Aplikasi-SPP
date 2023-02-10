72b621bfdc5c24dbd7cde398625a057f407cd03d
NauvalRafi <nauvalrafi590@gmail.com>
2023-02-09 20:21:51 -0800

    first commit

---
+++ Admin/Dashboard-Admin.php
@@ -0,0 +1,133 @@
+<?php
+include "../Template-SPP/Header.php";
+include "../Template-SPP/Navbar.php";
+include "../Template-SPP/Sidebar.php";
+?>
+      
+      
+      <!-- Main Content -->
+      <div class="main-content">
+        <section class="section">
+          <div class="row">
+            <div class="col-lg-12 col-md-12 col-sm-12">
+              <div class="card card-statistic-2">
+                <div class="card-stats">
+                  <div class="card-stats-title">DASHBOARD ADMIN
+              
+                    <div class="card-stats-items">
+                      <div class="card-stats-item">
+                        <div class="card-stats-item-label">Total Siswa</div>
+                        <div class="card-stats-item-count">36</div>
+                      </div>
+
+                      <div class="card-stats-item">
+                        <div class="card-stats-item-label">Total kelas</div>
+                        <div class="card-stats-item-count">3</div>
+                        
+                      </div>
+                      <div class="card-stats-item">
+                        <div class="card-stats-item-label">Total SPP</div>
+                        <div class="card-stats-item-count">23</div>
+                        
+                      </div>
+                    </div>
+                  </div>
+                  <div class="card-icon shadow-primary bg-primary">
+                    <i class="fas fa-archive"></i>
+                  </div>
+                  <div class="card-wrap">
+                  <div class="card-header">
+                    <h4>Data Transaksi Bulanan</h4>
+                  </div>
+                  <div class="card-body">
+                    59
+                  </div>
+                </div>
+              </div>
+            </div>
+            
+            
+         
+            
+                
+         
+          
+          <div class="row">
+            <div class="col-md-12">
+              <div class="card">
+                <div class="card-header">
+                  <h4>Data Transaksi Pembayaran SPP</h4>
+                  <div class="card-header-action">
+                  
+                  </div>
+                </div>
+                <div class="card-body p-0">
+                  <div class="table-responsive table-invoice">
+                    <table class="table table-striped">
+                      <tr>
+                        <th>#</th>
+                        <th>NISN</th>
+                        <th>Nama Siswa</th>
+                        <th>Tanggal Bayar</th>
+                        <th>Jumlah Bayar</th>
+                        <th>Petugas</th>
+                      </tr>
+                      <tr>
+                        <td><a href="#">1</a></td>
+                        <td class="font-weight-600">212210336</td>
+                        <td>ADNAN ALIF</td>
+                        <td>Jan 19, 2023</td>
+                        <td>850000</td>
+                        <td>
+                          <a href="#" class="btn btn-primary">Ujang</a>
+                        </td>
+                      </tr>
+                      <tr>
+                        <td><a href="#">2</a></td>
+                        <td class="font-weight-600">212210337</td>
+                        <td><div class="badge badge-success">ALBERTUS ICHVANIUS</div></td>
+                        <td>Jan 21, 2023</td>
+                        <td>750000</td>
+                        <td>
+                          <a href="#" class="btn btn-primary">Nita</a>
+                        </td>
+                      </tr>
+                      <tr>
+                        <td><a href="#">3</a></td>
+                        <td class="font-weight-600">212210338</td>
+                        <td><div class="badge badge-warning">ALYA QATRUN</div></td>
+                        <td>July 22, 2018</td>
+                        <td>550000</td>
+                        <td>
+                          <a href="#" class="btn btn-primary">Ujang</a>
+                        </td>
+                      </tr>
+                      <tr>
+                        <td><a href="#">3</a></td>
+                        <td class="font-weight-600">212210338</td>
+                        <td><div class="badge badge-warning">ALYA QATRUN</div></td>
+                        <td>July 22, 2018</td>
+                        <td>550000</td>
+                        <td>
+                          <a href="#" class="btn btn-primary">Ujang</a>
+                        </td>
+                      </tr>
+                      <tr>
+                        <td><a href="#">3</a></td>
+                        <td class="font-weight-600">212210338</td>
+                        <td><div class="badge badge-warning">ALYA QATRUN</div></td>
+                        <td>July 22, 2018</td>
+                        <td>550000</td>
+                        <td>
+                          <a href="#" class="btn btn-primary">Ujang</a>
+                        </td>
+                      </tr>
+                    </table>
+                  </div>
+                </div>
+              </div>
+            </div>
+           
+                  
+      
+<?php include "../Template-SPP/Footer.php"; ?>
